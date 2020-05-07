5bb00c6c1d66e35c8afad43ada895cc7695fcb26
Vladimir Turbin <vladimirturbin@gmail.com>
2020-05-07 07:58:43 +0300

    from now send data to tour.html template

--- tours/urls.py
+++ tours/urls.py
@@ -17,5 +17,5 @@ from django.urls import path
 from tours.views import DepartureView, TourView
 urlpatterns = [
     path('departure/<str:departure>/', DepartureView.as_view()),
-    path('tour/<int:id>/', TourView.as_view())
+    path('tour/<int:tour_id>/', TourView.as_view())
 ]
--- tours/views.py
+++ tours/views.py
@@ -1,20 +1,29 @@
 from django.shortcuts import render
 from django.views import View
 
+from tours.data import departures, tours
+
 
 class MainView(View):
-    def get(self, request, *args, **kwargs):
+    @staticmethod
+    def get(request, *args, **kwargs):
         context = {}
         return render(request, 'templates/index.html', context)
 
 
 class TourView(View):
-    def get(self, request, id, *args, **kwargs):
-        context = {}
+    @staticmethod
+    def get(request, tour_id, *args, **kwargs):
+        context = dict(tours[tour_id])
+        # Я исправил первую заглавную И поля 'departure' на строчную потому,
+        # что "Вьетнам из Екатеринбурга" выглядит эстетично,
+        # А "Вьетнам Из Екатеринбурга" Выглядит Как-То Глупо.
+        context['departure'] = 'и' + departures[context['departure']][1:]
         return render(request, 'templates/tour.html', context)
 
 
 class DepartureView(View):
-    def get(self, request, departure, *args, **kwargs):
+    @staticmethod
+    def get(request, departure, *args, **kwargs):
         context = {}
         return render(request, 'templates/departure.html', context)
